409c686fb81cdbe5a3f669b85ec03d6f8fcad555
Albert-Einholz <64534803+Albert-Einholz@users.noreply.github.com>
2023-07-17 02:53:59 -0400

    added ItemGroups - now uses static final instead of final static consistently

--- src/main/java/de/einholz/ehtech/registry/Registry.java
+++ src/main/java/de/einholz/ehtech/registry/Registry.java
@@ -3,6 +3,7 @@ package de.einholz.ehtech.registry;
 import de.einholz.ehmooshroom.gui.screen.ContainerScreen;
 import de.einholz.ehmooshroom.recipe.AdvRecipe;
 import de.einholz.ehmooshroom.registry.RegEntry;
+import de.einholz.ehtech.TechMod;
 import de.einholz.ehtech.block.MachineBlock;
 import de.einholz.ehtech.block.entity.CoalGeneratorBE;
 import de.einholz.ehtech.block.entity.OreGrowerBE;
@@ -19,21 +20,76 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.item.Item;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.util.Identifier;
 
 public class Registry {
-    // BLOCKS
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> CHARCOAL_BLOCK = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withBlockBuild(Block::new, FabricBlockSettings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(5.0F, 6.0F)).withBlockItemBuild(new Item.Settings()).withFuelBuild(16000).build("charcoal_block");
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> COKE_COAL_BLOCK = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withBlockBuild(Block::new, FabricBlockSettings.of(Material.STONE, MapColor.GRAY).requiresTool().strength(5.0F, 6.0F)).withBlockItemBuild(new Item.Settings()).withFuelBuild(32000).build("coke_coal_block");
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> MACHINE_FRAME_1 = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withBlockBuild(Block::new, MachineBlock.getDefaultSettings()).withBlockItemBuild(new Item.Settings()).build("machine_frame_1");
-    // MACHINES
-    public final static RegEntry<CoalGeneratorBE, CoalGeneratorGui, ContainerScreen<CoalGeneratorGui>, AdvRecipe> COAL_GENERATOR = RegistryTemplates.machine(new RegistryEntryBuilder<CoalGeneratorBE, CoalGeneratorGui, ContainerScreen<CoalGeneratorGui>, AdvRecipe>()).withBlockEntityBuild(CoalGeneratorBE::new).withGuiBuild(ExtendedScreenHandlerType<CoalGeneratorGui>::new, CoalGeneratorGui::init).withScreenBuild(ContainerScreen::new).withAdvRecipeBuild().build("coal_generator");
-    public final static RegEntry<OreGrowerBE, OreGrowerGui, ContainerScreen<OreGrowerGui>, AdvRecipe> ORE_GROWER = RegistryTemplates.machine(new RegistryEntryBuilder<OreGrowerBE, OreGrowerGui, ContainerScreen<OreGrowerGui>, AdvRecipe>()).withBlockEntityBuild(OreGrowerBE::new).withGuiBuild(ExtendedScreenHandlerType<OreGrowerGui>::new, OreGrowerGui::init).withScreenBuild(ContainerScreen::new).withAdvRecipeBuild().build("ore_grower");
-    // ITEMS
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> WRENCH = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withItemBuild(Wrench::new, new Item.Settings()).build("wrench");
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> HARD_COAL_TINY = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withItemBuild(Item::new, new Item.Settings()).withFuelBuild(200).build("hard_coal_tiny");
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> CHARCOAL_TINY = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withItemBuild(Item::new, new Item.Settings()).withFuelBuild(200).build("charcoal_tiny");
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> COKE_COAL_CHUNK = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withItemBuild(Item::new, new Item.Settings()).withFuelBuild(400).build("coke_coal_tiny");
-    public final static RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> COKE_COAL_TINY = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>().withItemBuild(Item::new, new Item.Settings()).withFuelBuild(3200).build("coke_coal_chunk");
+    public static final Identifier wrenchId = TechMod.HELPER.makeId("wrench");
 
-    public static void register() {}
+    // BLOCKS
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> CHARCOAL_BLOCK = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withBlockBuild(Block::new,
+                    FabricBlockSettings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(5.0F, 6.0F))
+            .withBlockItemBuild(new Item.Settings())
+            .withFuelBuild(16000)
+            .withItemGroupAddBuild(wrenchId)
+            .build("charcoal_block");
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> COKE_COAL_BLOCK = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withBlockBuild(Block::new,
+                    FabricBlockSettings.of(Material.STONE, MapColor.GRAY).requiresTool().strength(5.0F, 6.0F))
+            .withBlockItemBuild(new Item.Settings())
+            .withFuelBuild(32000)
+            .withItemGroupAddBuild(wrenchId)
+            .build("coke_coal_block");
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> MACHINE_FRAME_1 = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withBlockBuild(Block::new, MachineBlock.getDefaultSettings())
+            .withBlockItemBuild(new Item.Settings())
+            .withItemGroupAddBuild(wrenchId)
+            .build("machine_frame_1");
+
+    // MACHINES
+    public static final RegEntry<CoalGeneratorBE, CoalGeneratorGui, ContainerScreen<CoalGeneratorGui>, AdvRecipe> COAL_GENERATOR = RegistryTemplates
+            .machine(
+                    new RegistryEntryBuilder<CoalGeneratorBE, CoalGeneratorGui, ContainerScreen<CoalGeneratorGui>, AdvRecipe>())
+            .withBlockEntityBuild(CoalGeneratorBE::new)
+            .withGuiBuild(ExtendedScreenHandlerType<CoalGeneratorGui>::new, CoalGeneratorGui::init)
+            .withScreenBuild(ContainerScreen::new)
+            .withAdvRecipeBuild()
+            .build("coal_generator");
+    public static final RegEntry<OreGrowerBE, OreGrowerGui, ContainerScreen<OreGrowerGui>, AdvRecipe> ORE_GROWER = RegistryTemplates
+            .machine(new RegistryEntryBuilder<OreGrowerBE, OreGrowerGui, ContainerScreen<OreGrowerGui>, AdvRecipe>())
+            .withBlockEntityBuild(OreGrowerBE::new)
+            .withGuiBuild(ExtendedScreenHandlerType<OreGrowerGui>::new, OreGrowerGui::init)
+            .withScreenBuild(ContainerScreen::new)
+            .withAdvRecipeBuild()
+            .build("ore_grower");
+
+    // ITEMS
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> HARD_COAL_TINY = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withItemBuild(Item::new, new Item.Settings())
+            .withFuelBuild(200)
+            .withItemGroupAddBuild(wrenchId)
+            .build("hard_coal_tiny");
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> CHARCOAL_TINY = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withItemBuild(Item::new, new Item.Settings())
+            .withFuelBuild(200)
+            .withItemGroupAddBuild(wrenchId)
+            .build("charcoal_tiny");
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> COKE_COAL_CHUNK = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withItemBuild(Item::new, new Item.Settings())
+            .withFuelBuild(400)
+            .withItemGroupAddBuild(wrenchId)
+            .build("coke_coal_tiny");
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> COKE_COAL_TINY = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withItemBuild(Item::new, new Item.Settings())
+            .withFuelBuild(3200)
+            .withItemGroupAddBuild(wrenchId)
+            .build("coke_coal_chunk");
+
+    // ITEM GROUPS
+    public static final RegEntry<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>> WRENCH = new RegistryEntryBuilder<BlockEntity, ScreenHandler, HandledScreen<ScreenHandler>, Recipe<?>>()
+            .withSelfItemGroupBuild(Wrench::new, new Item.Settings())
+            .build("wrench");
+
+    public static void register() {
+    }
 }
--- src/main/java/de/einholz/ehtech/registry/RegistryTemplates.java
+++ src/main/java/de/einholz/ehtech/registry/RegistryTemplates.java
@@ -2,6 +2,7 @@ package de.einholz.ehtech.registry;
 
 import de.einholz.ehmooshroom.registry.RegEntryBuilder;
 import de.einholz.ehmooshroom.registry.RegTemplates;
+import de.einholz.ehtech.TechMod;
 import de.einholz.ehtech.block.MachineBlock;
 import de.einholz.ehtech.block.entity.MachineBE;
 import net.minecraft.block.entity.BlockEntity;
@@ -10,11 +11,20 @@ import net.minecraft.item.Item;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.screen.ScreenHandler;
 
-public class RegistryTemplates {
-    public static <B extends BlockEntity, G extends ScreenHandler, S extends HandledScreen<G>, R extends Recipe<?>> RegEntryBuilder<B, G, S, R> machine(RegEntryBuilder<B, G, S, R> entry) {
+public interface RegistryTemplates {
+    // XXX does this system in general make sense?
+    public static <B extends BlockEntity, G extends ScreenHandler, S extends HandledScreen<G>, R extends Recipe<?>> RegEntryBuilder<B, G, S, R> ehtech(
+            RegEntryBuilder<B, G, S, R> entry) {
+        entry.withItemGroupAddBuild(TechMod.HELPER.makeId("wrench"));
+        return entry;
+    }
+
+    public static <B extends BlockEntity, G extends ScreenHandler, S extends HandledScreen<G>, R extends Recipe<?>> RegEntryBuilder<B, G, S, R> machine(
+            RegEntryBuilder<B, G, S, R> entry) {
         entry.withBlockRaw((blockEntry) -> new MachineBlock(blockEntry.getId(), MachineBE::tick));
         RegTemplates.container(entry);
         entry.withBlockItemBuild(new Item.Settings());
+        ehtech(entry);
         return entry;
     }
 }
